44855986c4e6e076d223649a1d64cc9761160b02
jfmagauran <jfmagauran@gmail.com>
2016-04-24 16:56:00 -0400

    Added new class to allow the error to be written to a file.

---
+++ src/main/java/LogToFile.java
@@ -0,0 +1,75 @@
+/**
+ * LogToFile class
+ * This class is intended to be use with the default logging class of java
+ * It save the log in an XML file  and display a friendly message to the user
+ *
+ * @author Ibrabel <ibrabel@gmail.com>
+ */
+
+import javax.swing.*;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class LogToFile {
+
+    protected static final Logger logger = Logger.getLogger("MYLOG");
+
+    /**
+     * log Method
+     * enable to log all exceptions to a file and display user message on demand
+     *
+     * @param ex
+     * @param level
+     * @param msg
+     */
+    public void log(Exception ex, Severity level, String msg) {
+
+        FileHandler fh = null;
+        try {
+            fh = new FileHandler("log.xml", true);
+            logger.addHandler(fh);
+            switch (level) {
+                case SEVERE:
+                    logger.log(Level.SEVERE, msg, ex);
+                    if (!msg.equals(""))
+                        JOptionPane.showMessageDialog(null, msg,
+                                "Error", JOptionPane.ERROR_MESSAGE);
+                    break;
+                case WARNING:
+                    logger.log(Level.WARNING, msg, ex);
+                    if (!msg.equals(""))
+                        JOptionPane.showMessageDialog(null, msg,
+                                "Warning", JOptionPane.WARNING_MESSAGE);
+                    break;
+                case INFO:
+                    logger.log(Level.INFO, msg, ex);
+                    if (!msg.equals(""))
+                        JOptionPane.showMessageDialog(null, msg,
+                                "Info", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                case CONFIG:
+                    logger.log(Level.CONFIG, msg, ex);
+                    break;
+                case FINE:
+                    logger.log(Level.FINE, msg, ex);
+                    break;
+                case FINER:
+                    logger.log(Level.FINER, msg, ex);
+                    break;
+                case FINEST:
+                    logger.log(Level.FINEST, msg, ex);
+                    break;
+                //default:
+                //    logger.log(Level.CONFIG, msg, ex);
+                //    break;
+            }
+        } catch (IOException | SecurityException ex1) {
+            logger.log(Level.SEVERE, null, ex1);
+        } finally {
+            if (fh != null) fh.close();
+        }
+    }
+}
---
+++ src/main/java/Severity.java
@@ -0,0 +1,7 @@
+/**
+ * Created by jfm on 4/19/16.
+ */
+
+public enum Severity {
+    SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
+}
