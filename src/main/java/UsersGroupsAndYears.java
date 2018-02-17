e4acae4042693252fa91220c293ba2ac7dc5710b
patrick <patmagauran.j@gmail.com>
2018-02-16 21:02:23 -0500

    Added framework for new user management.
    Signed-off-by: patrick <patmagauran.j@gmail.com>

---
+++ src/main/java/UsersGroupsAndYears.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Patrick Magauran 2017.
+ *   Licensed under the AGPLv3. All conditions of said license apply.
+ *       This file is part of ABOS.
+ *
+ *       ABOS is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Affero General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       ABOS is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Affero General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Affero General Public License
+ *       along with ABOS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import javafx.stage.Window;
+
+public class UsersGroupsAndYears {
+
+    // --Commented out by Inspection START (1/2/2016 12:01 PM):
+//    /**
+//     * Launch the application.
+//     */
+//    public static void main(String Years, String[] args) {
+//        EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                try {
+//                    year = Years;
+//                    Year window = new Year(Years);
+//                    window.frame.setVisible(true);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+// --Commented out by Inspection STOP (1/2/2016 12:01 PM)
+    public UsersGroupsAndYears() {}
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    public void initUsersGroupsAndYears(Window parWindow) {
+    }
+
+
+}
+
