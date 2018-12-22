596c240b43ffe803e55ccfce82662c7c7b2862d2
patrick <patmagauran.j@gmail.com>
2018-12-21 21:10:50 -0500

    Remove Grails code

--- server/src/main/resources/db/migration/commons/V2.2.4__UpdateVersion.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `Settings` set `Value`='2.2.4' WHERE `key` = 'Version';
--- server/src/main/resources/db/migration/year/V2.2.4__ChangePaidToValue.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `orders`
-CHANGE COLUMN `paid` `paid` DECIMAL(9,2) NULL DEFAULT '0' ;
-
-UPDATE `orders` set `paid`=`Cost` WHERE `paid` = 1;
-UPDATE `orders` set `paid`=0 WHERE `paid` = 0;
-UPDATE `Settings` set `Value`='2.2.4' WHERE `key` = 'Version';
